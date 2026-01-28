f8f64b624d5ac133855bcf3aa9694d35757d3ea9
Ruan Pablo Furtado Oliveira <ruanpablooliveira20@gmail.com>
2026-01-28 17:09:21 -0300

    Adicionando mecânica de movimento

---
+++ Assets/Scripts/Player_Scripts/PlayerInteract.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteract : MonoBehaviour
+{
+   [Header("Configurações")]
+    public float interactRange = 1.5f;
+    public LayerMask interactLayer;
+
+    void OnInteract(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            TryInteract();
+        }
+    }
+
+    void TryInteract()
+    {
+        Collider2D foundObject = Physics2D.OverlapCircle(transform.position,interactRange,interactLayer);
+
+        if(foundObject != null)
+        {
+            IIteractable interactable = foundObject.GetComponent<IIteractable>();
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,interactRange);
+    }
+
+
+}
